b5cf52d8c379f4c98da19852b55f615911a0142f
SJP/farm <st33v@f3rr3t.com>
2017-11-22 22:09:00 +1100

    can copy and datestamp frog pix

--- look4frogs.sh
+++ look4frogs.sh
@@ -14,21 +14,75 @@
 #
 # This script is called by a timer service
 
-function createFrogLayer() {
-  convert pix/canhazfrogs.jpg -resize 500x500 -fx \
-  "hue>0.2 && hue<0.4 && saturation>0.2 && lightness>0.1 && lightness<0.9 ?1:0"\
-  -morphology Erode Octagon -despeckle pix/greenscreen.png
+# frog perception parameters
+maxHue=0.4  # based on a real frog
+#maxHue=0.6  # training with teapots for when there are no frogs
+minHue=0.2  # based on a real frog
+minSat=0.2  # minimum saturation
+minLight=0.15
+maxLight=0.95
+threshold=1 # minimum percentage of frog pixels
+thisDir="/home/st33v/shed/dev/frogcam/litoria"
+
+
+function gotodaydir {
+	yr=$(date +%Y)     # 4-digit year
+	mo=$(date +%m)
+	day=$(date +%d)
+	if [ ! -d ${yr}/${mo}/${day} ]; then
+		mkdir -p ${yr}/${mo}/${day}
+	fi
+	cd ${yr}/${mo}/${day}
+	# echo $PWD
+}
+
+function createFrogLayer {
+  convert pix/canhazfrogs.jpg -fx \
+  "hue>${minHue} && hue<${maxHue} && \
+  saturation>${minSat} && \
+  lightness>${minLight} && lightness<${maxLight} ?1:0"\
+  -morphology Erode:2 Octagon pix/greenscreen.png
 }
 
 # Is litoriaCam awake?
-ping litoriacam  -c 4 1>/dev/null
+function hazcam {
+  ping litoriacam  -c 1 1>/dev/null
+  if [[ $? -eq 0 ]] ; then
+      # echo "Cam is alive"
+      return 0
+  else
+      # echo "No response from litoriacam"
+      return 1
+  fi
+}
+
+# take a photo and look for frogs
+function findafrog {
+  curl litoriacam/image.jpg -o pix/canhazfrogs.jpg
+  createFrogLayer
+  # count the black (non-frog) and white (froggy) pixels
+  pixelHisto=$(convert pix/greenscreen.png -format %c histogram:info:-)
+  blackPixels=$(grep 000000 <<<$pixelHisto | awk '{print $1}' | sed 's/[^0-9]//g')
+  frogPixels=$(grep FFFFFF <<<$pixelHisto | awk '{print $1}' | sed 's/[^0-9]//g')
+  echo X$frogPixelsX$blackPixelsX
+  totalPixels=$((frogPixels+blackPixels))
+  echo ${totalPixels}
+  percent=$(awk "BEGIN { pc=100*${frogPixels}/${totalPixels}; \
+          i=int(pc); print (pc-i<0.5)?i:i+1 }")
+  echo ${pixelHisto}
+  echo frog ${frogPixels} non-frog ${blackPixels} ratio  ${percent}
+  if [[ $percent -gt $threshold ]] ; then
+    return 0
+  else
+    return 1  # no frogs today
+  fi
+}
+
+cd $thisDir		#need to be explicit when the script is run in /usr/local/bin
+hazcam
+findafrog
 if [[ $? -eq 0 ]] ; then
-    echo "I can see you"
-    curl litoriacam/image.jpg -o pix/canhazfrogs.jpg
-    createFrogLayer
-    greenPix=$(convert pix/greenscreen.png -format %c histogram:info:- |\
-     grep 000000 | awk '{print $1}' | sed 's/[^0-9]//g')
-    echo ${greenPix}
-else
-    echo "No response from litoriacam"
+  #here=$PWD
+  # gotodaydir  # cd to a directory
+  cp pix/canhazfrogs.jpg frogs/$(date +%Y-%m-%d_%H%M%S).jpg
 fi
